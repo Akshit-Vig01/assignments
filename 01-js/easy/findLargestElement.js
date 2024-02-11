3392033e0b71a57aa789c5caa0a6346745d7c74e
Akshit-Vig01 <156829501+Akshit-Vig01@users.noreply.github.com>
2024-02-11 16:41:07 +0530

    Update findLargestElement.js

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,17 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let max = 0;
+    for (let i=0; i<numbers.length;i++){
+        if (numbers[i]> max){
+             max = numbers[i];
+        }          
+    }  
+    return max;
 }
+const numbers = [3, 7, 2, 9, 1]
+let sabtovaddaelement = findLargestElement(numbers)
+console.log(sabtovaddaelement)
+
 
 module.exports = findLargestElement;
